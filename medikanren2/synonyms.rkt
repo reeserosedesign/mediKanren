861f39e731a26c6155f41b17a53db97252728e71
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-10-28 10:09:19 -0700

    synonyms.rkt: comment out code that does not run in production

--- medikanren2/synonyms.rkt
+++ medikanren2/synonyms.rkt
@@ -11,11 +11,15 @@
   json
   memoize)
 
-(provide direct-synonym direct-synonym* direct-synonym+ synonym close-match-synonym
+(provide (all-defined-out)
+         #|
+         direct-synonym direct-synonym* direct-synonym+ synonym close-match-synonym
          synonyms/step synonym-of/step synonyms/breadth synonym-of/breadth
          synonym synonym-path kgx-synonym
          simple-synonym
-         get-synonyms-ls get-names-ls get-names-set)
+         get-synonyms-ls get-names-ls get-names-set
+         |#
+         )
 
 (define (equivalence-relation node link)
   (define-relation (equiv=/= a b)
@@ -120,6 +124,7 @@
 (define (non-empty-intersection set1 set2)
   (not (set-empty? (set-intersect set1 set2))))
 
+
 (define-relation (close-match-synonym a b)
   (fresh (id cat-a cat-b)
     (edge id a b)
@@ -136,6 +141,7 @@
                    ((:== #t (a) (string-prefix? a "NCBIGene:")))
                    ((:== #t (a) (string-prefix? a "UniProtKB:")))
                    ((:== #t (a) (string-prefix? a "ENSEMBL:"))))))))
+#|
 
 (define-relation (direct-synonym* a b)
   (conde ((== a b))
@@ -216,6 +222,7 @@
   (let ((synonyms (set->list (synonyms/step term n))))
     (relation synonym-of/step^ (s)
       (membero s synonyms))))
+|#
 
 (define (synonyms/breadth term (n 2) (categories '()))
   (let loop ((n (- n 1)) (synonyms (set term)) (terms (list term)) )
@@ -243,10 +250,12 @@
 ;; relation for use in mk queries
 ;; n is required
 ;; (run* s ((synonym-of/breadth autism 2) s)))
+#|
 (define/memo* (synonym-of/breadth term n)
   (let ((synonyms (synonyms/breadth term n)))
     (relation synonym-of/breadth^ (s)
       (membero s synonyms))))
+|#
 
 ;; used in TRAPI interpreter
 ;; implements basic logic: KGX-syn for genes/proteins, and KGs for others
@@ -261,6 +270,7 @@
                                (run* s (kgx-synonym curie s)))))
                        curies)))))
 
+#|
 ; get-names-ls function takes a list of curies and return the curies with their coresponding names
 (define (get-names-ls curie-ls)
   (run* (curie name)
@@ -269,6 +279,6 @@
 
 (define (get-names-set curie-set)
   (get-names-ls (set->list curie-set)))
-
+|#
 
   
